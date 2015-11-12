57cdeb903e5cb3940f86bbc5826f4b04ee88e095
Joe Ferris <jferris@thoughtbot.com>
2015-11-12 14:39:07 -0500

    Add specs and stub methods for exericse

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,4 +3,20 @@ class Person < ActiveRecord::Base
   belongs_to :role
   belongs_to :manager, class_name: "Person", foreign_key: :manager_id
   has_many :employees, class_name: "Person", foreign_key: :manager_id
+
+  def self.order_by_location_name
+    all
+  end
+
+  def self.with_employees
+    all
+  end
+
+  def self.without_employees
+    all
+  end
+
+  def self.with_employees_order_by_location_name
+    all
+  end
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,4 +1,85 @@
 require "spec_helper"
 
 describe Person do
+  describe ".order_by_location_name" do
+    it "groups people by location" do
+      pending "Fix this spec first"
+
+      locations = [
+        create(:location, name: "location1"),
+        create(:location, name: "location3"),
+        create(:location, name: "location2")
+      ]
+      locations.each do |location|
+        create(:person, location: location, name: "at-#{location.name}")
+      end
+
+      result = Person.order_by_location_name
+
+      expect(result.map(&:name)).
+        to eq(%w(at-location1 at-location2 at-location3))
+    end
+  end
+
+  describe ".with_employees" do
+    it "finds people who manage employees" do
+      pending "Fix this spec second"
+
+      managers = [
+        create(:person, name: "manager-one"),
+        create(:person, name: "manager-two")
+      ]
+      managers.each do |manager|
+        2.times do
+          create(:person, name: "employee-of-#{manager.name}", manager: manager)
+        end
+      end
+
+      result = Person.with_employees
+
+      expect(result.map(&:name)).to match_array(%w(manager-one manager-two))
+    end
+  end
+
+  describe ".without_employees" do
+    it "finds people who don't manage any employees" do
+      pending "Fix this spec third"
+
+      manager = create(:person, name: "manager")
+      create(:person, manager: manager, name: "employee-one")
+      create(:person, manager: manager, name: "employee-two")
+
+      result = Person.without_employees
+
+      expect(result.map(&:name)).to match_array(%w(employee-one employee-two))
+    end
+  end
+
+  describe ".with_employees_order_by_location_name" do
+    it "finds managers ordered by location name" do
+      pending "Fix this spec last"
+
+      locations = [
+        create(:location, name: "location1"),
+        create(:location, name: "location3"),
+        create(:location, name: "location2")
+      ]
+      managers = locations.map do |location|
+        create(:person, name: "manager-at-#{location.name}", location: location)
+      end
+      managers.each do |manager|
+        2.times do
+          create(:person, name: "employee-of-#{manager.name}", manager: manager)
+        end
+      end
+
+      result = Person.with_employees_order_by_location_name
+
+      expect(result.map(&:name)).to eq(%w(
+        manager-at-location1
+        manager-at-location2
+        manager-at-location3
+      ))
+    end
+  end
 end
